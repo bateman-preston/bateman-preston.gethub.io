b0f8ed35c6abed4ac97bd996119501edcffee9b9
bateman-preston <bat190152byui.edu>
2020-02-01 15:45:18 -0700

    IT

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -1,3 +1,15 @@
 function toggleMenu() {
     document.querySelector(".activenav").classList.toggle("responding")
 }
+function lastUpdated() {
+    let d = new Date(document.lastModified);
+    document.getElementById("spanLastUpdated").innerHTML = "Last updated: " + d.toLocaleString();
+  }
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.getElementById('today').innerHTML = today
