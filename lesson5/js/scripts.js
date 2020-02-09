d1a518ad7953443aae60ca69e63127172d8d8653
bateman-preston <bat190152byui.edu>
2020-02-08 20:19:11 -0700

    push

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -10,6 +10,18 @@ var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
+let month = date.toLocaleString('default', { month: 'long' });
+console.log(month);
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
 
-today = mm + '/' + dd + '/' + yyyy;
+var n = weekday[today.getDay()];
+
+today = n + ',' + dd + " " + month + " " + yyyy;
 document.getElementById('today').innerHTML = today
