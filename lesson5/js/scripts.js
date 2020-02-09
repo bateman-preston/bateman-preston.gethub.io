fefd16188d98d12005e7ddfd8ad4f840d50baeaf
bateman-preston <bat190152byui.edu>
2020-02-09 00:47:51 -0700

    yep

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -1,27 +1,73 @@
+update();
 function toggleMenu() {
     document.querySelector(".activenav").classList.toggle("responding")
 }
 
-let d = new Date(document.lastModified);
-document.getElementById("lastUpdated").innerHTML = "Last updated: " + d.toLocaleString();
+function update(){
+    let today = new Date();
+    var day;
+    var month;
+    switch (today.getDay()) {
+        case 0:
+          day = "Sunday, ";
+          break;
+        case 1:
+          day = "Monday, ";
+          break;
+        case 2:
+          day = "Tuesday, "
+          break;
+        case 3:
+          day = "Wednesday, ";
+          break;
+        case 4:
+          day = "Thursday, "
+          break;
+        case 5:
+          day = "Friday, "
+          break;
+        case 6:
+          day = "Saturday, "
+          break;
+      }
+      switch (today.getMonth()) {
+        case 0:
+          month = " January ";
+          break;
+        case 1:
+          month = " Feburary ";
+          break;
+        case 2:
+          month = " March "
+          break;
+        case 3:
+          month = " April ";
+          break;
+        case 4:
+          month = " May "
+          break;
+        case 5:
+          month = " June "
+          break;
+        case 6:
+          month = " July "
+          break;
+        case 7:
+          month = " August "
+          break;
+        case 8:
+          month = " September ";
+          break;
+        case 9:
+          month = " October "
+          break;
+        case 10:
+          month = " Novemeber "
+          break;
+        case 11:
+          month = " December "
+          break;
+      }
+    document.getElementById("today").innerHTML = "Last Updated: " + day + today.getDate() + month + today.getFullYear();
+}
 
-
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-let month = date.toLocaleString('default', { month: 'long' });
-console.log(month);
-var weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
-
-var n = weekday[today.getDay()];
-
-today = n + ',' + dd + " " + month + " " + yyyy;
-document.getElementById('today').innerHTML = today
