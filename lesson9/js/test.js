c520c01644e7c078456c90f0ed1c2f5a9cfa7b30
bateman-preston <bat190152byui.edu>
2020-03-08 01:10:48 -0700

    push

---
+++ lesson9/js/test.js
@@ -0,0 +1,52 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject); // temporary checking for valid response and data parsing
+
+        const towns = jsonObject['towns']; //create an array constant
+        for (let i = 0; i < towns.length; i++) { //iterate through the array
+            if (towns[i].name == "Preston" || towns[i].name == "Fish Haven" || towns[i].name == "Soda Springs") {
+                //creating the elements example: <h2></h2>
+                let card = document.createElement('section');
+                card.classList.add('card');
+                let h2 = document.createElement('h2');
+                let h3 = document.createElement('h3');
+                let div = document.createElement('div');
+                let founded = document.createElement('span');
+                let population = document.createElement('span');
+                let rain = document.createElement('span');
+                let image = document.createElement('img');
+                let br = document.createElement('br');
+                let br2 = document.createElement('br');
+                let br3 = document.createElement('br');
+
+                //setting the inner text example: <h2>CONTENT</h2>
+                h2.textContent = towns[i].name;
+                h3.textContent = towns[i].motto;
+                founded.textContent = "Year Founded: " + towns[i].yearFounded;
+                population.textContent = "Population: " + towns[i].currentPopulation;
+                rain.textContent = "Average Rain Fall: " + towns[i].averageRainfall;
+
+                //add the created element to it's parent
+                card.appendChild(div);
+                div.appendChild(h2);
+                div.appendChild(h3);
+                div.appendChild(founded);
+                div.appendChild(br);
+                div.appendChild(population);
+                div.appendChild(br2);
+                div.appendChild(rain);
+                card.appendChild(br3);
+                card.appendChild(image); //added this
+
+                //add card to the card container
+                document.querySelector('section.cards').appendChild(card);
+                //set the img src and alt
+                image.setAttribute('src', "img/" + towns[i].photo);
+                image.setAttribute('alt', h2.textContent) //added this
+            }
+        }
+    });
---
+++ lesson9/js/towns.js
@@ -0,0 +1,49 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject){
+        console.table(jsonObject);
+
+        const towns = jsonObject['towns'];
+        for (let i = 0; i < towns.length; i++) {
+            if (towns[i].name == "Preston" || towns[i].name == "Fish Haven" || towns[i].name == "Soda Springs") {
+                let card = document.createElement('section');
+                card.classList.add('card');
+                let h1 = document.createElement('h1');
+                let h3 = document.createElement('h3');
+                let div = document.createElement('div');
+                let founded = document.createElement('span');
+                let population = document.createElement('span');
+                let rain = document.createElement('span');
+                let image = document.createElement('img');
+                let br = document.createElement('br');
+                let br2 = document.createElement('br');
+                let br3 = document.createElement('br');
+                let br4 = document.createElement('br');
+
+                h1.textContent = towns[i].name;
+                h3.textContent = towns[i].motto;
+                founded.textContent = "Founded In: " + towns[i].yearFounded;
+                population.textContent = "Current Population: " + towns[i].currentPopulation;
+                rain.textContent = "Yearly Average Rainfall: " = towns[i].averageRainfall;
+
+                card.appendChild(div);
+                div.appendChild(h1);
+                div.appendChild(h3);
+                div.appendChild(founded);
+                div.appendChild(br);
+                div.appendChild(br2);
+                div.appendChild(population);
+                div.appendChild(br3);
+                div.appendChild(rain);
+                div.appendChild(br4);
+                div.appendChild(image);
+
+                document.querySelector('section.cards').appendChild(card);
+                image.setAttribute('src', 'img/' + towns[i].photo);
+                image.setAttribute('alt', h1.textContent);
+            }
+        }
+    });
