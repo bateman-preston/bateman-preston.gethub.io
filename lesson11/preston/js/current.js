651183b855cb1352a1dade06db9800b640255757
bateman-preston <bat190152byui.edu>
2020-03-21 11:40:50 -0600

    pushy

---
+++ lesson11/preston/js/current.js
@@ -0,0 +1,26 @@
+const requestURL1 = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=8105380f08037194620e8fd4e23cde13";
+    fetch(requestURL1)
+    .then(function (response) {
+        return response.json();
+        })
+    .then (function (currentWeather){
+        console.log(currentWeather);
+        const temp = currentWeather.main.temp;
+        let tempToday = document.querySelector('#temp');
+        tempToday.innerHTML = temp;
+
+        const humidity = currentWeather.main.humidity;
+        let humToday = document.querySelector('#humidity');
+        humToday.innerHTML = humidity;
+
+        const wind = currentWeather.wind.speed;
+        let speedToday = document.querySelector('#wind');
+        speedToday.innerHTML = wind;
+
+        const pressure = currentWeather.main.pressure;
+        let pressureToday = document.querySelector('#pressure');
+        pressureToday.innerHTML = pressure;
+
+        console.log(tempToday.innerHTML);
+    
+        });
